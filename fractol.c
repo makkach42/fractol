0e818719eb6425d4f4c67f4258d8d7ba92c6782e
Mohamed amine Akkach <makkach@e3r7p6.1337.ma>
2025-02-22 19:40:16 +0100

    fractol

--- fractol.c
+++ fractol.c
@@ -1,44 +1,68 @@
 #include "fractol.h"
 
-// int func(int key)
-// {
-//     if (key == 53)
-//         exit(0);
-//     return (0);
-// }
-
-    // t_window *window;
-
-    // window = malloc(sizeof(t_window));
-    // window->mlx = mlx_init();
-    // window->width = 800;
-    // window->hight = 600;
-    // window->win = mlx_new_window(window->mlx, window->width, window->hight, "window");
-    // window->x = 200;
-    // window->y = 200;
-    // int k = 600;
-    // int l = 400;
-    // int m = 200;
-
-    // while (window->y < l)
-    // {
-    //     window->x = m;
-    //     while (window->x < k)
-    //     {
-    //         mlx_pixel_put(window->mlx, window->win, window->x, window->y, 0xFFF0000);
-    //         window->x++;
-    //     }
-    //     m++;
-    //     k--;
-    //     window->y++;
-    // }
-    // mlx_key_hook(window->win, func, window);
-
-    // mlx_loop(window->mlx);
 double scaling_func(double num, double new_min, double new_max, double old_min, double old_max)
 {
     return ((new_max - new_min) * (num - old_min) / (old_max - old_min) + new_min);
 }
+
+void    pixel_put(int x, int y, t_image *img, int color)
+{
+    int offset;
+
+    offset = (y * img->line_len) + (x *(img->bpp / 8));
+    *(unsigned int *)(img->pixel + offset) = color;
+}
+void    handl_pixel(double x, double y, t_window *window)
+{
+    t_complex   z;
+    t_complex   c;
+    double      tmpreal;
+    int         i;
+    int         color;
+
+    z.real = 0;
+    z.imaginary = 0;
+    c.real = scaling_func(x, -2, 2, 0, WIDTH);
+    c.imaginary = scaling_func(y, 2, -2, 0, HEIGHT);
+    tmpreal = 0;
+    i = 0;
+    while (i < ITER_MAX)
+    {
+        tmpreal = (z.real * z.real) - (z.imaginary * z.imaginary) + c.real;
+        z.imaginary = 2 * z.real * z.imaginary + c.imaginary;
+        z.real = tmpreal;
+        if ((z.imaginary * z.imaginary) + (z.real * z.real) > 4)
+        {
+            color = scaling_func(i, 0x000000, 0xFFFFFF, 0, ITER_MAX);
+            pixel_put(x, y, &window->image, color);
+            return ;
+        }
+        i++;
+    }
+    pixel_put(x, y, &window->image, 0x000000);
+
+}
+void fractal_render(t_window *window)
+{
+    int x;
+    int y;
+
+    x = 0;
+    y = 0;
+    while (y < HEIGHT)
+    {
+        x = 0;
+        while (x < WIDTH)
+        {
+            handl_pixel(x, y, window);
+            x++;
+        }
+        y++;
+    }
+    mlx_put_image_to_window(window->mlx, window->win, window->image.image, 0, 0);
+}
+
+
 void    window_init(t_window *window)
 {
     window->mlx = mlx_init();
@@ -52,50 +76,21 @@ void    window_init(t_window *window)
         free(window->mlx);
         exit(1);
     }
-    window->image->image = mlx_new_image(window->mlx, WIDTH, HEIGHT);
-    if (!window->image->image)
+    window->image.image = mlx_new_image(window->mlx, WIDTH, HEIGHT);
+    if (!window->image.image)
     {
         mlx_destroy_window(window->mlx, window->win);
         free(window->mlx);
         exit(1);
     }
-    window->image->pixel = mlx_get_data_addr(window->image->image, &window->image->bpp, &window->image->line_len, 0);
-    //hooks;
+    window->image.pixel = mlx_get_data_addr(window->image.image, &window->image.bpp, &window->image.line_len, &window->image.endian);
 }
 int main()
 {
-//     t_complex z;
-//     t_complex c;
+    t_window window;
 
-//     z.real = 0;
-//     z.imaginary = 0;
-
-//     c.real = 0;
-//     c.imaginary = 0;
-//     int i = 0;
-//     double tmpreal;
-//     while (i < 10)
-//     {
-//         tmpreal = (z.real * z.real) - (z.imaginary * z.imaginary);
-//         z.imaginary = 2 * z.real * z.imaginary;
-//         z.real = tmpreal;
-//         z.real += c.real;
-//         z.imaginary += c.imaginary;
-//         printf("iteration %d, z.real = %f, z.imaginary = %f\n", i, z.real,z.imaginary);
-//         i++;
-//     }
-
-//  init
-
-// render
-
-//  mlx_loop
-    int i = 0;
-    while (i < 8)
-    {
-        printf("%d, %f\n", i, scaling_func(i, -2, 2, 0, 799));
-        i++;
-    }
-
+    window_init(&window);
+    fractal_render(&window);
+    mlx_loop((&window)->mlx);
     return (0);
 }
--- fractol.h
+++ fractol.h
@@ -2,30 +2,34 @@
 # define FRACTOL_H
 # include <stdio.h>
 # include <stdlib.h>
-# include <mlx.h>
+# include "minilibx_opengl/mlx.h"
 
 # define WIDTH 800
 # define HEIGHT 800
+# define ITER_MAX 100
 typedef struct s_image
 {
     void    *image;
     char    *pixel;
     int     bpp;
     int     line_len;
+    int     endian;
 }   t_image;
-
-typedef struct s_window
-{
-    void    *mlx;
-    void    *win;
-    t_image *image;
-}   t_window;
 
 typedef struct s_complex
 {
     double real;
     double imaginary;
 }   t_complex;
+
+typedef struct s_window
+{
+    void        *mlx;
+    void        *win;
+    t_complex   *coords;
+    t_image     image;
+}   t_window;
+
 
 int julia_check(char **argv);
 int mandelbrot_check(char *argv);
