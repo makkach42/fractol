aa4c2079311795048ff4f1c500f948181f4e60e1
Mohamed amine Akkach <makkach@e3r10p14.1337.ma>
2025-02-25 08:52:35 +0000

    fract-ol

--- bonus/fractol_bonus.c
+++ bonus/fractol_bonus.c
@@ -17,6 +17,10 @@ int main(int argc, char **argv) //range check //remove + - keys
 
         window.julia.real = atodbl(argv[2]);
         window.julia.imaginary = atodbl(argv[3]);
+        if (window.julia.real > 2 || window.julia.real < -2)
+            return (0);
+        if (window.julia.imaginary > 2 || window.julia.imaginary < -2)
+            return (0);
         window.name = "julia";
         window_init(&window);
         fractal_render(&window);
